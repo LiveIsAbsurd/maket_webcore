6e9021f45299e2080d4702bea3ed15485cf87f69
LiveIsAbsurd <anndreybykov19@gmail.com>
2023-05-03 00:11:43 +0300

    Нейминг классов

--- script.js
+++ script.js
@@ -10,19 +10,19 @@ if (window.innerWidth < 500) {
   })
 }
 
-var brandListButton = document.querySelector('.brand-list-open-button');
+var brandListButton = document.querySelector('.brand-list-button');
 var brandList = document.querySelector('.brand-list');
 
 brandListButton.addEventListener('click', function () {
   if (brandList.classList.contains('brand-list--close')) {
     brandList.classList.remove('brand-list--close');
     brandList.classList.add('brand-list--open');
-    brandListButton.classList.add('brand-list-open-button--open');
+    brandListButton.classList.add('brand-list-button--open');
     brandListButton.textContent = 'Скрыть';
   } else {
     brandList.classList.add('brand-list--close');
     brandList.classList.remove('brand-list--open');
-    brandListButton.classList.remove('brand-list-open-button--open');
+    brandListButton.classList.remove('brand-list-button--open');
     brandListButton.textContent = 'Показать все';
   }
 });
